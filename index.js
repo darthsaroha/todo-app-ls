d92f90b67b0ed5f7cc88f2cd0244bba9b25c7139
darthsaroha <darthsaroha@gmail.com>
2018-07-30 11:56:53 +0530

    initial

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 3000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
